a5f524823647cc4460a33ddff2fc5f6c7f195591
student <yosefisraelgoren@gmail.com>
2021-06-17 12:12:29 +0000

    added tests for Queue

--- freetest/quick.c
+++ freetest/quick.c
@@ -3,7 +3,7 @@
 #include "../webserver-files/Queue.h"
 #include "assert.h"
 
-int main(){
+void test1(){
 	Queue* q = initQ(3);
 	assert(emptyQ(q));
 	
@@ -27,7 +27,39 @@ int main(){
 	assert(emptyQ(q));
 
 	destroyQ(q);
-	printf("test successfull!\n");
+	printf("test1 success!\n");
+}
+
+void test2(){
+	static const int numq = 10;
+	Queue* q = initQ(4);
+	assert(emptyQ(q));
+	enqueueQ(q, 200);
+	enqueueQ(q, 100);
+	enqueueQ(q, 50);
+	enqueueQ(q, 25);
+	assert(fullQ(q));
+	assert(dequeueQ(q) == 200);
+	assert(dequeueQ(q) == 100);
+	assert(dequeueQ(q) == 50);
+	assert(dequeueQ(q) == 25);
+	assert(emptyQ(q));
+
+	enqueueQ(q, 10);
+	enqueueQ(q, 20);
+	assert(dequeueQ(q) == 10);
+	enqueueQ(q, 30);
+	enqueueQ(q, 40);
+	assert(dequeueQ(q) == 20);
+	destroyQ(q);
+	
+	printf("test2 success!\n");
+}
+
+int main(){
+	test1();
+	test2();
+
 	return 0;
 }
 
