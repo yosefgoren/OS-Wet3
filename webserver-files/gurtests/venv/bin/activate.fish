a8a41b1b62f4d33b4658451f80f4e29dfc4fc589
student <yosefisraelgoren@gmail.com>
2021-06-19 08:44:44 +0000

    reinitialized corrupt repository

--- webserver-files/gurtests/venv/bin/activate.fish
+++ webserver-files/gurtests/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/student/hw3/webserver-files/gurtests/venv"
+set -gx VIRTUAL_ENV "/home/student/newhw3/OS-Wet3/webserver-files/gurtests/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
